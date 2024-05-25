39f6fd168d1480fdfc722ecfd2a0889aaef9315a
227Faddi <falukhouma27@gmail.com>
2024-05-25 09:19:03 -0400

    8kyu

---
+++ JavaScript/8kyu/Remove-exclamation-marks.js
@@ -0,0 +1,22 @@
+// DESCRIPTION:
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// My Solution
+
+// P - Parameters
+// a string
+
+// R - Return
+// the string without any exclamation marks
+
+// E - Examples
+// 'hey!' => 'hey'
+
+// P - Pseudocode
+// use split to transform the string into an array then map or filter the array then join to get back to a satring
+
+function removeExclamationMarks(s) {
+    return s.split('').filter((item) => item !== '!').join('');
+}
+
+console.log(removeExclamationMarks('hey!!'))
