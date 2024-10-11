b32334b236f65b7cc39656308a460ba6bc111558
227Faddi <falukhouma27@gmail.com>
2024-10-11 09:57:23 -0400

    7kyu

---
+++ JavaScript/7kyu/Substring-fun.js
@@ -0,0 +1,24 @@
+// Description:
+// Complete the function that takes an array of words.
+
+// You must concatenate the nth letter from each word to construct a new word which should be returned as a string, where n is the position of the word in the list.
+
+// For example:
+
+// ["yoda", "best", "has"]  -->  "yes"
+//   ^        ^        ^
+//   n=0     n=1     n=2
+// Note: Test cases contain valid input only - i.e. a string array or an empty array; and each word will have enough letters.
+
+function nthChar(words){
+    if(words.length === 0){
+        return ''
+    }
+    let result = ''
+    words.map((word, index) => result += word[index])
+    return result
+}
+
+console.log(nthChar(['yoda', 'best', 'has'])) // 'yes'
+console.log(nthChar([])) // 'yes'
+
