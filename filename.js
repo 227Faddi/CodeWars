57340f3142c987b11a5796aca4db0a0b7d5bf482
227Faddi <falukhouma27@gmail.com>
2024-11-05 10:18:39 -0500

    7kyu

---
+++ filename.js
@@ -0,0 +1,5 @@
+function formatFileName(string){
+    console.log(`${string.replaceAll(' ', '-')}.js`)
+}
+
+formatFileName('Coding Meetup #11 - Higher-Order Functions Series - Find the average age')
