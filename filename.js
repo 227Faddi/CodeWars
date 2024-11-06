63e8c312ec0cc747e65d1d6017a539a0c530355c
227Faddi <falukhouma27@gmail.com>
2024-11-06 11:06:03 -0500

    7kyu

--- filename.js
+++ filename.js
@@ -2,4 +2,4 @@ function formatFileName(string){
     console.log(`${string.replaceAll(' ', '-')}.js`)
 }
 
-formatFileName('Coding Meetup #11 - Higher-Order Functions Series - Find the average age')
+formatFileName('Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins')
