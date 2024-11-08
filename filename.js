c9e08cc6870616ba25b21b83c32035756af6fb97
227Faddi <falukhouma27@gmail.com>
2024-11-07 21:36:35 -0500

    7kyu

--- filename.js
+++ filename.js
@@ -2,4 +2,4 @@ function formatFileName(string){
     console.log(`${string.replaceAll(' ', '-')}.js`)
 }
 
-formatFileName('Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins')
+formatFileName('Jenny the youngest detective')
