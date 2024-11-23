d83aa9309eae9653d8201b3e97d32db3f3e48625
227Faddi <falukhouma27@gmail.com>
2024-11-22 20:44:18 -0500

    feat: complete 7kyu kata

--- filename.js
+++ filename.js
@@ -2,6 +2,4 @@ function formatFileName(string) {
   console.log(`${string.replaceAll(" ", "-")}.js`);
 }
 
-formatFileName(
-  "Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe"
-);
+formatFileName("The Office III - Broken Photocopier");
