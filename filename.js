5fa45a4a05e702964b44b197d5d0b2c04829a244
227Faddi <falukhouma27@gmail.com>
2024-12-06 09:04:56 -0500

    feat: 7kyu kata complete

--- filename.js
+++ filename.js
@@ -2,6 +2,4 @@ function formatFileName(string) {
   console.log(`${string.replaceAll(" ", "-")}.js`);
 }
 
-formatFileName(
-  "Coding Meetup #16 - Higher-Order Functions Series - Ask for missing details"
-);
+formatFileName("Sort Santa's Reindeer");
