9c74d469318825e8c2beb8016949a078ac564f3d
227Faddi <falukhouma27@gmail.com>
2024-12-14 20:20:36 -0500

    feat: 8kyu kata complete

--- filename.js
+++ filename.js
@@ -2,4 +2,4 @@ function formatFileName(string) {
   console.log(`${string.replaceAll(" ", "-")}.js`);
 }
 
-formatFileName("Sort Santa's Reindeer");
+formatFileName("Find the first non-consecutive number");
