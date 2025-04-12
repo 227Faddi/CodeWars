8a6d8ec0cfffa663a29767d026b1e61f7e2d4ef9
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2025-04-12 10:48:27 -0400

    Add solution - CodeHub

---
+++ 8-kyu/return-the-day/return-the-day.py
@@ -0,0 +1,18 @@
+def whatday(num: int):
+    week = {
+        1:"Sunday",
+        2: "Monday",
+        3: "Tuesday",
+        4: "Wednesday",
+        5: "Thursday",
+        6: "Friday",
+        7: "Saturday",
+    }
+    
+    if num > 7 or num < 1:
+        return "Wrong, please enter a number between 1 and 7"
+    else:
+        return week[num]
+​
+    
+    
