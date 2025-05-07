42b079929a0ded3f4d3e0b6aa27d1f93db67e3d7
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2025-05-07 16:28:45 -0400

    Add solution - CodeHub

---
+++ 8-kyu/price-of-mangoes/price-of-mangoes.js
@@ -0,0 +1,15 @@
+function mango(quantity, price){
+  // 2 integers
+  // the number , total price
+  //check 
+  let tot = quantity * price
+  
+  
+  
+  let bonus = Math.floor(quantity / 3) * price
+  
+  return tot - bonus
+  
+  
+​
+}
